35a09ed29a791a782cfb8ffaab7eccd72303ca02
Tynho <Tynho@10.10.130.67>
2019-10-19 14:54:07 -0300

    aa

--- CombustivelControl/src/aulas/Principal.java
+++ CombustivelControl/src/aulas/Principal.java
@@ -1,10 +1,52 @@
 package aulas;
 
+import aula.InputStream;
+import aula.Scanner;
+
 class principal {
 
 	public static void main(String[] args) {
 
-		calcularAutonomiaMaster(300, 50);
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("Sistema de Controle de Combustível");
+			System.out.println("1-Entrada de dados:");
+			System.out.println("2-Relatórios:");
+			System.out.println("3-Sair");
+			
+			Scanner sc = new Scanner (System.in);
+			
+			op = sc.nextint();
+			
+			if (op ==1) {
+				System.out.println("Digite Nome");
+				String nome = sc.next();
+				
+				System.out.println("KM Rodado");
+				
+				float kmrodado = sc.nextFloat("KM Rodado")
+						
+				
+				float litros = sc.netxfloat();
+				
+				
+			}
+			
+			
+		}
+
+		private static void Scanner(InputStream in) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		private static int nextint() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+	}
+
 
 	}
 }
