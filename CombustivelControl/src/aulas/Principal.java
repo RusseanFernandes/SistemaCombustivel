9f439f9cf3520b9ce83ee975cb370149ab078cee
Russean <Russean@10.10.149.232>
2019-10-18 21:04:45 -0300

    testestes

--- CombustivelControl/src/aulas/Principal.java
+++ CombustivelControl/src/aulas/Principal.java
@@ -5,16 +5,11 @@ class principal {
 	public static void main(String[] args) {
 		// meu primeiro commit
 		// valeu time
-<<<<<<< HEAD
-		System.out.println("Olá turma");
-		System.out.println("teste");
-
-=======
-		System.out.println("OlÃ¡ turma");
+		System.out.println("Ola! turma");
 		System.out.println("GITHUB");
 		System.out.println("GIT");
 		System.out.println("aula");
->>>>>>> branch 'master' of https://github.com/RusseanFernandes/SistemaCombustivel.git
+		System.out.println("21:04");
 	}
 
 }
