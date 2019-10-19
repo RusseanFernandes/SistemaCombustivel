2d1ebd67c50cb9c693dd1c34090dabdbdddc85b1
Russean <Russean@10.10.149.232>
2019-10-19 14:21:04 -0300

    Calculo de Autonomia

--- CombustivelControl/src/aulas/Principal.java
+++ CombustivelControl/src/aulas/Principal.java
@@ -3,14 +3,10 @@ package aulas;
 class principal {
 
 	public static void main(String[] args) {
-		// meu primeiro commit
-		// valeu time
-		System.out.println("Ola! turma");
-		System.out.println("GITHUB");
-		System.out.println("GIT");
-		System.out.println("aula");
-		System.out.println("21:04");
-		System.out.println("21:07");
-	}
 
+
+		
+		
+	}
+	
 }
---
+++ CombustivelControl/src/aulas/calcularAutonomia.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class calcularAutonomia {
+
+	public float calcularAutonomia(float km, float litros){
+		float Autonomia= 0.0F;
+			
+			Autonomia= km/litros;
+			return Autonomia;	
+	}
+}
