1568f06e970d212f0e94c513dac541e08d4db456
DEV <DEV@DEV-PC>
2019-10-20 00:56:59 -0300

    alteracao

--- CombustivelControl/src/aulas/Principal.java
+++ CombustivelControl/src/aulas/Principal.java
@@ -6,33 +6,37 @@ class principal {
 
 	public static void main(String[] args) {
 
-		
-			// TODO Auto-generated method stub
-			System.out.println("Sistema de Controle de Combustível");
-			System.out.println("1-Entrada de dados:");
-			System.out.println("2-Relatórios:");
-			System.out.println("3-Sair");
-			
-			Scanner sc = new Scanner (System.in);
-			
-			int op = sc.nextInt();
-			
-			if (op ==1) {
-				System.out.println("Digite Nome");
-				String nome = sc.next();
-				
-				System.out.println("KM Rodado");
-				
-				float kmrodado = sc.nextFloat();
-						
-				System.out.println("digite o litro");
-				float litros = sc.nextFloat();
-				
-				//teste novo
-			}
-			
-			
+		// TODO Auto-generated method stub
+		System.out.println("Sistema de Controle de Combustível");
+		System.out.println("1-Entrada de dados:");
+		System.out.println("2-Relatórios:");
+		System.out.println("3-Sair");
+
+		Scanner sc = new Scanner(System.in);
+
+		int op = sc.nextInt();
+
+		if (op == 1) {
+			System.out.println("Digite Nome");
+			String nome = sc.next();
+
+			System.out.println("KM Rodado");
+
+			float kmrodado = sc.nextFloat();
+
+			System.out.println("digite o litro");
+			float litros = sc.nextFloat();
+
+			// teste novo
+		} else if (op == 2) {
+
+			System.out.println("PERFEITO");
+		} else {
+
+			System.out.println("INPERFEITO");
+
 		}
 
-		
+	}
+
 }
